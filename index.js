6142bc925efc58d4fe4570af3ea792421a3cc751
gauravpp <gaurav.palav@gmail.com>
2018-05-15 08:02:37 +0530

    j

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+
+
 app.get('/', function(request, response) {
-  response.send('Captain America!!!')
+  
+  // Network interfaces
+var ifaces = require('os').networkInterfaces();
+
+// Iterate over interfaces ...
+var adresses = Object.keys(ifaces).reduce(function (result, dev) {
+  return result.concat(ifaces[dev].reduce(function (result, details) {
+    return result.concat(details.family === 'IPv4' && !details.internal ? [details.address] : []);
+  }, []));
+});
+
+// Print the result
+console.log(adresses);
+  response.send(adresses);
 })
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
